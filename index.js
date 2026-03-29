0d3effc0c87afc0f33b1e7266b178996808bef84
Rajaram <rmohanroyr@gmail.com>
2026-03-29 12:03:17 +0530

    adde file

--- index.js
+++
@@ -1,9 +0,0 @@
-const http = require("http");
-
-const server = http.createServer((req, res) => {
-  res.end("🔥 DevOps Pipeline Working!");
-});
-
-server.listen(3000, () => {
-  console.log("Server running on port 3000");
-});
